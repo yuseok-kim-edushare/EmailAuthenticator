875361a4e42b7d16d344a4ef47ca80af40a02e18
김유석(Yu Seok Kim) <yuseok.kim@edushare.kr>
2024-08-08 22:39:04 +0900

    v.o.o.1
    implement Auth feature
    (need to call method with service provider's email end password in string)

---
+++ src/main/java/com/kys9808/EmailAuth/Auth.java
@@ -0,0 +1,28 @@
+package com.kys9808.EmailAuth;
+
+import java.time.LocalDateTime;
+import java.util.Random;
+
+class Authenticating {
+    public static String generateAuthCode() {
+        // Generate a random 7-digit authentication code
+        Random random = new Random();
+        int randomNumber;
+        randomNumber = 10000000 + random.nextInt(90000000);
+        return "S" + String.valueOf(randomNumber);
+    }
+}
+
+public class Auth {
+    public static AuthJob AuthStart(String userEmail, String serviceProviderEmail, String serviceProviderPassword) {
+        String authChallenge = Authenticating.generateAuthCode();
+        AuthJob AuthJob = new AuthJob(userEmail, "second", LocalDateTime.now().plusMinutes(5));
+        authChallenge = "your authentication code is: " + authChallenge;
+        EmailSender.sendEmail(userEmail, "Authentication Code", authChallenge, serviceProviderEmail, serviceProviderPassword);
+        return AuthJob;
+    }
+    public static boolean AuthCheck(AuthJob AuthJob, String authCode) {
+        return LocalDateTime.now().isBefore(AuthJob.getExpireTime()) && AuthJob.getAuthcode().equals(authCode);
+    }
+}
+
